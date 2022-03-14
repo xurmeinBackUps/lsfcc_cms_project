8eaa10b3e4d759a2a2975a063fbdb598c3846ea0
xurmeinBackUps <wj.pavocrist@gmail.com>
2022-03-14 12:16:53 -0400

    first; maybe not doing rails app, idk...

---
+++ db/migrate/20220314132343_create_stories.rb
@@ -0,0 +1,11 @@
+class CreateStories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.string :author
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
